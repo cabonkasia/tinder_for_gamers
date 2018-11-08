37951ca96bfd5b0b1ccef6b4870ca1027b2ee766
checolori <dario.antonioli@gmail.com>
2018-11-08 12:27:47 +0100

    fixed style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import heart from "../heart.png";
-import "../Style.css";
+import '../styles/style.css'
 import dislike from "../dislike.png";
 import profile from "../profile.png";
 
@@ -8,11 +8,10 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer className="footer">
-        <button><img src={heart} className="button" /></button>
-
-        <button><img src={dislike} className="button" /></button>
-        <button><img src={profile} className="button" /></button>
-
+        <button><img src={dislike} className="buttonFooter" /></button>
+        <button><img src={profile} className="buttonFooter" /></button>
+        <button><img src={heart} className="buttonFooter" /></button>
+        
       </footer>
     );
   }
