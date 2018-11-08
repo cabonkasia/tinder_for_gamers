f007651c32a58e1412573bfa9312c6099e79ee74
checolori <dario.antonioli@gmail.com>
2018-11-08 09:55:07 +0100

    fix footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer className="footer">
-        <button><img src={heart} className="button" /></button>
 
-        <button><img src={dislike} className="button" /></button>
-        <button><img src={profile} className="button" /></button>
+        <button><img src={dislike} className="buttonFooter" /></button>
+        <button><img src={profile} className="buttonFooter" /></button>
+        <button><img src={heart} className="buttonFooter" /></button>
 
       </footer>
     );
