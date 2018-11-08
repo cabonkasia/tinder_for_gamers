1d1fdf5e3367ccc924da2e9065664af1a5a9ba5f
KarelReitsma <reitsma.karel@gmail.com>
2018-11-08 17:18:03 +0100

    Added dumb matchList.js and added it to the messages page. Also created an empty matchListContainer.js

--- src/pages/messagespage.js
+++ src/pages/messagespage.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import NavBar from '../components/NavBar'
-// import message from './components/message'
+import MatchList from '../components/matchList'
 
 export default function MessagesPage(props) {
   return (
     <div>
       <NavBar />
-      {/* Messages comp */}
+      <MatchList />
     </div>
   )
 }
